cd59ebd935eb9039731ab2abcc6ddd4101f683b4
MusieYemane <m80007278@gmail.com>
2022-03-14 15:42:36 -0500

    Remove cart

--- ProductService/src/main/java/com/product/productservice/repository/ProductRepository.java
+++ ProductService/src/main/java/com/product/productservice/repository/ProductRepository.java
@@ -2,8 +2,6 @@ package com.product.productservice.repository;
 
 import com.product.productservice.domain.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
