37da9538bfbefea46f0994275bc98d84822f08ae
MusieYemane <m80007278@gmail.com>
2022-03-14 17:51:31 -0500

    Added to Eureka server as client

--- ProductService/src/main/java/com/product/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.product.productservice.repository;
-
-import com.product.productservice.domain.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
