142d7205000da8a2542d93873d70f1482a3f36c5
Mike <michalerezene@gmail.com>
2022-03-15 15:06:31 -0500

    mergedAll

--- ShoppingService/src/main/java/Shopping/ShoppingService/ShoppingServiceApplication.java
+++ ShoppingService/src/main/java/Shopping/ShoppingService/ShoppingServiceApplication.java
@@ -2,12 +2,10 @@ package Shopping.ShoppingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 @EnableKafka
-@EnableDiscoveryClient
 public class ShoppingServiceApplication {
 
 	public static void main(String[] args) {
