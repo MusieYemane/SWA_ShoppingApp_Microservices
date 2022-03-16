103dab1f50b7c40b15b950aed4ccf1a93676f11e
MusieYemane <m80007278@gmail.com>
2022-03-16 12:19:39 -0500

    add logger info

--- ProductService1/src/main/java/com/product/productservice/controller/ProductController.java
+++ ProductService1/src/main/java/com/product/productservice/controller/ProductController.java
@@ -28,33 +28,38 @@ public class ProductController {
 
     @GetMapping
     public ResponseEntity<Products> getAllProducts(){
-        logger.info("Calling Products");
+        logger.info("Calling get all Products");
         return ResponseEntity.ok(productService.getAllProducts());
     }
 
     @GetMapping("/{productNumber}")
     public ResponseEntity<Product> getProductById(@PathVariable("productNumber") String productNumber){
+        logger.info("Calling get product by id");
         return ResponseEntity.ok(productService.getProductById(productNumber));
     }
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        logger.info("Calling add product by id");
         return ResponseEntity.ok(productService.addProduct(product));
     }
 
     @DeleteMapping("/{productNumber}")
     public void deleteProductById(@PathVariable("productNumber") String productNumber){
+        logger.info("Calling delete product by id");
         productService.deleteProductById(productNumber);
     }
 
     @PutMapping("/{productNumber}")
     public ResponseEntity<Product> editProduct(@PathVariable String productNumber,
                                                @RequestBody Product product){
+        logger.info("Calling edit product by id");
         return ResponseEntity.ok(productService.editProduct(productNumber, product));
     }
 
     @GetMapping("/{productNumber}/isInStock")
     public ResponseEntity<Boolean> productIsInStock(@PathVariable String productNumber){
+        logger.info("Calling get product in stock");
         if (productService.getProductNumInStock(productNumber)>0) return ResponseEntity.ok(true);
         return ResponseEntity.ok(false);
     }
